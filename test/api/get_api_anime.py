dfa87e791e38320c76fc632f658a22e440800dbf
vvfkill <me34on34@mail.ru>
2026-09-22 22:01:49 +0300

    modified

--- test/api/get_api_anime.py
+++ test/api/get_api_anime.py
@@ -1,16 +1,16 @@
 from playwright.sync_api import sync_playwright
 
-url = "http://127.0.0.1:8000/api/anime/?page=1&page_size=10"
+url = "http://127.0.0.1:8000/api/anime/"
 
 def test_get_api_anime():
     with sync_playwright() as p:
 
         request = p.request.new_context() #создаю APIRequestContext
-
-        response = request.get(url)
-
+        response = request.get(
+            url, 
+            params = {"page": 1, "page_size": 10})
+        
         data = response.json() #метод
-
         assert response.status == 200
 
         assert "page" in data
@@ -39,5 +39,7 @@ def test_get_api_anime():
           
             for genre in anime["genres"]:
                 assert isinstance(genre, str)
+
+        print(data)
 
         request.dispose()
